bb14e29231975326bc49c5644d9470c141c33004
dmitrykarpenko <dmitry.karpenko@gmail.com>
2016-12-06 15:32:15 +0200

    DependencyInjector (CR): redundant usings removed

--- School.CompositionRoot/DependencyInjector.cs
+++ School.CompositionRoot/DependencyInjector.cs
@@ -3,12 +3,7 @@ using Autofac.Integration.Mvc;
 using School.DataLayer.Abstract;
 using School.DataLayer.Concrete;
 using School.Logic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace School.CompositionRoot
